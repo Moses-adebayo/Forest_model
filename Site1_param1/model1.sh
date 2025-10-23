0948f415d14b5b269d7ca073a74b7130242ee49d
Moses Adebayo (Student) <moses_adebayo@mines.edu>
2025-10-23 00:20:18 -0600

    starting deterministic inversion

---
+++ Site1_param1/model1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH -A CLI185
+#SBATCH -N 1
+#SBATCH -J Site_1
+#SBATCH --mail-user=moses_adebayo@mines.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:45:00
+#SBATCH -p batch
+srun -n 50 ats --xml_file=../Site1_param1.xml &> out.log
+python post_run.py
